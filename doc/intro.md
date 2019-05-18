02831a9d962ed30a677b04df4c1dce3e4467505f
Faris Nasution <electric.ladyland156@gmail.com>
2019-05-19 06:12:46 +0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gilmour
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ modules/aleph/src/gilmour/aleph.clj
@@ -0,0 +1,18 @@
+(ns gilmour.aleph
+  (:require
+   [aleph.http :refer [start-server]]
+   [com.stuartsierra.component :as c]))
+
+(defrecord HttpServer [config handler server]
+  c/Lifecycle
+  (start [this]
+    (let [handler (:handler handler handler)
+          server  (start-server handler config)]
+      (assoc this :server server)))
+  (stop [this]
+    (when server (.close server))
+    (assoc this :server nil)))
+
+(defn make-http-server
+  [config]
+  (map->HttpServer {:config config}))
