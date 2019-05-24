eda838beee482c91cf318ea53129ade1b30e913c
Faris Nasution <electric.ladyland156@gmail.com>
2019-05-25 06:58:42 +0700

    Bump version. Correcting target repo

--- modules/bidi/project.clj
+++ modules/bidi/project.clj
@@ -1,11 +1,11 @@
-(defproject gilmour/bidi "0.1.8"
+(defproject gilmour/bidi "0.1.9"
   :description "Bidi as component"
   :url "https://github.com/nasutiooon/gilmour/tree/master/modules/bidi"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[bidi "2.1.6"]
                  [metosin/ring-http-response "0.9.1"]]
-  :repositories [["releases" {:url   "https://repo.clojars.org"
+  :repositories [["releases" {:url   "https://clojars.org/repo"
                               :creds :gpg}]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0-beta3"]
                                   [com.stuartsierra/component "0.4.0"]]}})
