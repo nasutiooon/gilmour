b8b13e081a8d14ae6e2ab14702fac9fe24c5e725
Faris Nasution <electric.ladyland156@gmail.com>
2019-05-25 06:05:20 +0700

    Drop config

--- modules/hikari/src/gilmour/hikari.clj
+++ modules/hikari/src/gilmour/hikari.clj
@@ -3,14 +3,14 @@
    [hikari-cp.core :as hikari]
    [com.stuartsierra.component :as c]))
 
-(defrecord Hikari [config datasource]
+(defrecord Hikari [datasource]
   c/Lifecycle
   (start [this]
-    (assoc this :datasource (hikari/make-datasource config)))
+    (assoc this :datasource (hikari/make-datasource this)))
   (stop [this]
     (when datasource (hikari/close-datasource datasource))
     (assoc this :datasource nil)))
 
 (defn make-hikari
   [config]
-  (map->Hikari {:config config}))
+  (map->Hikari config))
