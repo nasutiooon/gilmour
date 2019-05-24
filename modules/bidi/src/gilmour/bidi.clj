d1abcb16443dea60870a2c012f9ebb6bebd90b55
Faris Nasution <electric.ladyland156@gmail.com>
2019-05-24 22:27:23 +0700

    Drop ResourceProvider

--- modules/bidi/src/gilmour/bidi.clj
+++ modules/bidi/src/gilmour/bidi.clj
@@ -5,32 +5,18 @@
    [com.stuartsierra.component :as c]
    [ring.util.http-response :refer [not-found]]))
 
-(defprotocol ResourceProvider
-  (resources [this]))
-
-(defn- search-resource-hooks
-  [component]
-  (or (->> (vals component)
-           (filter #(and (satisfies? b/RouteProvider %)
-                         (satisfies? ResourceProvider %)))
-           (not-empty))
-      (throw (ex-info "Can't find any attached resource" {}))))
-
-(defrecord Router [routes not-found-handler handler]
+(defrecord Router [routes routes-fn resources-fn not-found-handler handler]
   b/RouteProvider
   (routes [_] routes)
 
   c/Lifecycle
   (start [this]
-    (let [hooks     (search-resource-hooks this)
-          routes    (->> hooks
-                         (map b/routes)
-                         (reduce merge {}))
-          resources (->> hooks
-                         (map resources)
-                         (reduce merge {}))
+    (let [routes    (routes-fn this)
+          resources (when resources-fn (resources-fn this))
           handler   (some-fn
-                     (make-handler ["" routes] resources)
+                     (if resources
+                       (make-handler ["" routes] resources)
+                       (make-handler ["" routes]))
                      (or not-found-handler (constantly (not-found))))]
       (assoc this :routes routes :handler handler)))
   (stop [this]
