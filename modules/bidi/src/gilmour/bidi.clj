7d905cae3bef6a9e5ffdbb808ebe38e16df66ac7
Faris Nasution <electric.ladyland156@gmail.com>
2019-05-24 21:24:10 +0700

    Make use of ResourceProvider

--- modules/bidi/src/gilmour/bidi.clj
+++ modules/bidi/src/gilmour/bidi.clj
@@ -5,23 +5,36 @@
    [com.stuartsierra.component :as c]
    [ring.util.http-response :refer [not-found]]))
 
-(defrecord Router [routes resources not-found-handler handler]
+(defprotocol ResourceProvider
+  (resources [this]))
+
+(defn- search-resource-hooks
+  [component]
+  (or (->> (vals component)
+           (filter #(and (satisfies? b/RouteProvider %)
+                         (satisfies? ResourceProvider %)))
+           (not-empty))
+      (throw (ex-info "Can't find any attached resource" {}))))
+
+(defrecord Router [routes not-found-handler handler]
   b/RouteProvider
   (routes [_] routes)
 
   c/Lifecycle
   (start [this]
-    (let [routes  (if (fn? routes) (routes this) routes)
-          handler (some-fn
-                   (if resources
-                     (make-handler ["" routes] (if (fn? resources)
-                                                 (resources this)
-                                                 resources))
-                     (make-handler ["" routes]))
-                   (or not-found-handler (constantly (not-found))))]
-      (assoc this :handler handler)))
+    (let [hooks     (search-resource-hooks this)
+          routes    (->> hooks
+                         (map b/routes)
+                         (reduce merge {}))
+          resources (->> hooks
+                         (map resources)
+                         (reduce merge {}))
+          handler   (some-fn
+                     (make-handler ["" routes] resources)
+                     (or not-found-handler (constantly (not-found))))]
+      (assoc this :routes routes :handler handler)))
   (stop [this]
-    (assoc this :handler nil)))
+    (assoc this :routes nil :handler nil)))
 
 (defn make-router
   [opts]
