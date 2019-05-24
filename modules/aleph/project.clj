c962c163cfedb9528f94bd148999308f7538cd29
Faris Nasution <electric.ladyland156@gmail.com>
2019-05-24 19:40:47 +0700

    Bump version

--- modules/aleph/project.clj
+++ modules/aleph/project.clj
@@ -1,4 +1,4 @@
-(defproject gilmour/aleph "0.1.3"
+(defproject gilmour/aleph "0.1.4"
   :description "Aleph as component"
   :url "https://github.com/nasutiooon/gilmour/tree/master/modules/aleph"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
