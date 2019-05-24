4f753aeac4049ca6d941494976a696a5c66ab3d8
Faris Nasution <electric.ladyland156@gmail.com>
2019-05-25 06:04:05 +0700

    Bump version

--- modules/datomic/project.clj
+++ modules/datomic/project.clj
@@ -1,4 +1,4 @@
-(defproject gilmour/datomic "0.1.6"
+(defproject gilmour/datomic "0.1.7"
   :description "Datomic as component"
   :url "https://github.com/nasutiooon/gilmour/tree/master/modules/datomic"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
