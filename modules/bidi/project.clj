87ce3e73043103d4eac61dcb3c7cccd70b5b2ddb
Faris Nasution <electric.ladyland156@gmail.com>
2019-05-24 22:27:37 +0700

    Bump version

--- modules/bidi/project.clj
+++ modules/bidi/project.clj
@@ -1,4 +1,4 @@
-(defproject gilmour/bidi "0.1.5"
+(defproject gilmour/bidi "0.1.6"
   :description "Bidi as component"
   :url "https://github.com/nasutiooon/gilmour/tree/master/modules/bidi"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
