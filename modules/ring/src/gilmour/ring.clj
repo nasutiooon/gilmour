6fd2a21bc9bbfe4b8cda93355d5643fc401d98fa
Faris Nasution <electric.ladyland156@gmail.com>
2019-05-28 06:59:52 +0700

    RingHandler -> RingHead. Remove RingMiddleware

--- modules/ring/src/gilmour/ring.clj
+++ modules/ring/src/gilmour/ring.clj
@@ -8,38 +8,22 @@
 (defprotocol RequestMiddleware
   (request-middleware [this]))
 
-(defrecord RingHandler [middleware-kws]
+(defrecord RingHead []
   RequestHandler
   (request-handler [this]
-    (let [handler    (or (->> (vals this)
-                              (filter (partial satisfies? RequestHandler))
-                              (map request-handler)
-                              (first))
-                         (throw
-                          (ex-info "Ring handler requires a request handler"
-                                   {})))
-          middleware (let [ms (if (seq middleware-kws)
-                                (reduce (fn [c kw]
-                                          (if-let [v (get this kw)]
-                                            (conj c v)
-                                            c))
-                                        []
-                                        middleware-kws)
-                                (vals this))]
-                       (->> ms
-                            (filter (partial satisfies? RequestMiddleware))
-                            (map request-middleware)
-                            (apply comp)))]
-      (middleware handler))))
+    (let [handler    (->> (vals this)
+                          (filter (partial satisfies? RequestHandler))
+                          (map request-handler)
+                          (first))
+          middleware (->> (vals this)
+                          (filter (partial satisfies? RequestMiddleware))
+                          (map request-middleware)
+                          (apply comp))]
+      (fn [request]
+        (let [new-handler (middleware handler)
+              new-request (assoc request :component this)]
+          (new-handler new-request))))))
 
-(defn make-ring-handler
-  [config]
-  (map->RingHandler config))
-
-(defrecord RingMiddleware [wrapper]
-  RequestMiddleware
-  (request-middleware [this] (wrapper this)))
-
-(defn make-ring-middleware
-  [config]
-  (map->RingMiddleware config))
+(defn make-ring-head
+  []
+  (map->RingHead {}))
