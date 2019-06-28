a9e6c59c585fc4e59f19dd8e4e29fac046d4b0b3
Faris Nasution <electric.ladyland156@gmail.com>
2019-06-28 19:06:30 +0700

    Update postgres

--- modules/postgres/src/gilmour/postgres.clj
+++ modules/postgres/src/gilmour/postgres.clj
@@ -32,15 +32,13 @@
 
 (defn create!
   [{:keys [pool-spec]}]
-  (when pool-spec
-    (with-open [conn (j/connection (pool-spec->postgres-db-spec pool-spec))]
-      (let [db-name (:database-name pool-spec)]
-        (j/execute conn (str "DROP DATABASE IF EXISTS " db-name))
-        (j/execute conn (str "CREATE DATABASE " db-name))))))
+  (with-open [conn (j/connection (pool-spec->postgres-db-spec pool-spec))]
+    (let [db-name (:database-name pool-spec)]
+      (j/execute conn (str "DROP DATABASE IF EXISTS " db-name))
+      (j/execute conn (str "CREATE DATABASE " db-name)))))
 
 (defn destroy!
   [{:keys [pool-spec]}]
-  (when pool-spec
-    (with-open [conn (j/connection (pool-spec->postgres-db-spec pool-spec))]
-      (let [db-name (:database-name pool-spec)]
-        (j/execute conn (str "DROP DATABASE " db-name))))))
+  (with-open [conn (j/connection (pool-spec->postgres-db-spec pool-spec))]
+    (let [db-name (:database-name pool-spec)]
+      (j/execute conn (str "DROP DATABASE " db-name)))))
