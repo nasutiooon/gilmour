ad9c563d34c28534d19842bef2815bfd7ccbce6b
Faris Nasution <electric.ladyland156@gmail.com>
2019-07-16 14:23:48 +0700

    Bump version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/nasutiooon/gilmour"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0-beta3"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :managed-dependencies [[org.clojure/clojure "1.10.0-beta3"]
                          [com.stuartsierra/component "0.4.0"]
                          [gilmour/ring "0.1.9"]
