b3f7e486370e100f18b24bc3c55f14134b37c79d
Faris Nasution <electric.ladyland156@gmail.com>
2019-07-29 16:10:03 +0700

    Update postgres

--- modules/postgres/src/gilmour/postgres.clj
+++ modules/postgres/src/gilmour/postgres.clj
@@ -1,20 +1,16 @@
 (ns gilmour.postgres
   (:require
+   [clojure.string :as string]
    [com.stuartsierra.component :as c]
    [gilmour.sql :as g.sql]
    [jdbc.core :as j]))
 
 (defrecord PGPool []
-  g.sql/SQLPool
-  (pool [this]
+  g.sql/SQLDb
+  (db-spec [this]
     (->> (vals this)
-         (filter (partial satisfies? g.sql/SQLPool))
-         (map g.sql/pool)
-         (first)))
-  (pool-spec [this]
-    (->> (vals this)
-         (filter (partial satisfies? g.sql/SQLPool))
-         (map g.sql/pool-spec)
+         (filter (partial satisfies? g.sql/SQLDb))
+         (map g.sql/db-spec)
          (first))))
 
 (defn pg-pool
@@ -25,55 +21,42 @@
   [ex]
   (.getSQLState (.getServerErrorMessage ex)))
 
-(defn db-spec
-  [{:keys [adapter server-name port-number database-name username password]}]
-  (cond-> {:subprotocol adapter
-           :subname     (str "//" server-name ":" port-number "/" database-name)}
-    username (assoc :user username)
-    password (assoc :password password)))
+(defn- get-db-name
+  [db-spec]
+  (let [splitted (some-> db-spec :subname (string/split #"\/"))]
+    (when (= (count splitted) 4)
+      (last splitted))))
 
-(defn pg-db-spec
-  [spec]
-  (db-spec (assoc spec :database-name "postgres")))
+(defn- pg-db-spec
+  [db-spec]
+  (let [splitted (some-> db-spec :subname (string/split #"\/"))]
+    (when (= (count splitted) 4)
+      (assoc db-spec :subname (-> (take 3 splitted)
+                                  (vec)
+                                  (conj "postgres")
+                                  (as-> <> (string/join "/" <>)))))))
 
-(defrecord DurablePGGenerator [pool-spec]
-  c/Lifecycle
-  (start [this]
-    (with-open [conn (j/connection (pg-db-spec pool-spec))]
-      (let [db-name (:database-name pool-spec)]
-        (when (nil? (j/fetch-one conn (str "SELECT datname "
-                                           "FROM pg_catalog.pg_database "
-                                           "WHERE datname = '"
-                                           db-name
-                                           "'")))
-          (j/execute conn (str "CREATE DATABASE " db-name)))
-        this)))
-  (stop [this]
-    this))
+(defrecord PGGenerator [db-spec])
 
-(defn durable-pg-generator
-  [config]
-  (map->DurablePGGenerator config))
+(defn create!
+  [{:keys [db-spec]}]
+  (with-open [conn (j/connection (pg-db-spec db-spec))]
+    (let [db-name (get-db-name db-spec)]
+      (when (nil? (j/fetch-one conn (str "SELECT datname "
+                                         "FROM pg_catalog.pg_database "
+                                         "WHERE datname = '"
+                                         db-name
+                                         "'")))
+        (j/execute conn (str "CREATE DATABASE " db-name))))))
 
-(defrecord EphemeralPGGenerator [pool-spec]
-  c/Lifecycle
-  (start [this]
-    (with-open [conn (j/connection (pg-db-spec pool-spec))]
-      (let [db-name (:database-name pool-spec)]
-        (j/execute conn (str "DROP DATABASE IF EXISTS " db-name))
-        (j/execute conn (str "CREATE DATABASE " db-name)))
-      this))
-  (stop [this]
-    (with-open [conn (j/connection (pg-db-spec pool-spec))]
-      (j/execute conn (str "DROP DATABASE " (:database-name pool-spec)))
-      this)))
+(defn create-fresh!
+  [{:keys [db-spec]}]
+  (with-open [conn (j/connection (pg-db-spec db-spec))]
+    (let [db-name (get-db-name db-spec)]
+      (j/execute conn (str "DROP DATABASE IF EXISTS " db-name))
+      (j/execute conn (str "CREATE DATABASE " db-name)))))
 
-(defn ephemeral-pg-generator
-  [config]
-  (map->EphemeralPGGenerator config))
-
-(defn pg-generator
-  [{:keys [temporary?] :as config}]
-  (if temporary?
-    (ephemeral-pg-generator config)
-    (durable-pg-generator config)))
+(defn destroy!
+  [{:keys [db-spec]}]
+  (with-open [conn (j/connection (pg-db-spec db-spec))]
+    (j/execute conn (str "DROP DATABASE IF EXISTS " (get-db-name db-spec)))))
